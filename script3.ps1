a25d5fb7b11879a8441cee00f3897ad2c709f0be
AzureShruthi <azure.shruthi@gmail.com>
2022-01-16 11:53:52 +0530

    This is another file

---
+++ script3.ps1
@@ -0,0 +1,20 @@
+﻿$i = 1
+do{
+write "value of $i"
+$i++
+write "Now the valus of $i"
+}while($i -lt 20)
+
+
+$i=0
+while($i -lt 20)
+{
+$i++
+write "Now the value of $i"
+}
+
+$i=1
+do{ 
+$i++
+write "Now the value of $i"
+} until ($i -eq 10)
